bef9505d0267c0ad8ecbab72f5c5d963ad8e1577
Sakana727 <22020383@rp.edu.sg>
2023-08-18 11:45:48 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22020383/demoweather/Weather.java
@@ -0,0 +1,37 @@
+package sg.edu.rp.c346.id22020383.demoweather;
+
+import androidx.annotation.NonNull;
+
+public class Weather {
+
+    private String area;
+    private String forecast;
+
+    public Weather(String area, String forecast) {
+        this.area = area;
+        this.forecast = forecast;
+    }
+
+    public String getForecast() {
+        return forecast;
+    }
+
+    public void setForecast(String forecast) {
+        this.forecast = forecast;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "WEATHER\r\nArea: " + area + "\r\nForecast: " + forecast;
+    }
+}
